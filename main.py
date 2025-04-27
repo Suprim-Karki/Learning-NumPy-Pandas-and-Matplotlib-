9501e003506ddcc4c4609a9b9e7975738f41d340
Suprim-Karki <shupreem1911@gmail.com>
2025-04-27 19:48:50 +0545

    Different array functions

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import numpy as np
+
+arr=np.array([[1,2,3],
+               [4,5,6],
+               [7,8,9]])
+
+print(arr[0])
+print(arr[0,1])
+
+print(arr.shape)     #gives 3,3 as in 3 rows 3 columns
+print(arr.ndim)      #gives dimension of the array (2 because 2 layers)
+print(arr.size)      #total number of elements
+print(arr.dtype)     #gives data types
