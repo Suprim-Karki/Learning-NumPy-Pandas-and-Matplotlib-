919b7c947005e2fd5f3b3f6aadcdad304f70fedc
Suprim-Karki <shupreem1911@gmail.com>
2025-04-27 22:00:49 +0545

    Array methods

--- main.py
+++ main.py
@@ -34,24 +34,40 @@ import numpy as np
 
 '''Mathematical operations'''
 
-l1=[1,2,3,4,5]
-l2=[6,7,8,9,0]
+# l1=[1,2,3,4,5]
+# l2=[6,7,8,9,0]
 
-a1=np.array(l1)
-a2=np.array(l2)
+# a1=np.array(l1)
+# a2=np.array(l2)
 
-print(l1*5)   #prints the list 5 times
-print(a1*5)   #prints the list with its items multiplied by 5
-print(a1+5)   #this is not possible in normal list
-print(a1-5)   
+# print(l1*5)   #prints the list 5 times
+# print(a1*5)   #prints the list with its items multiplied by 5
+# print(a1+5)   #this is not possible in normal list
+# print(a1-5)   
 
-print(l1+l2)  #concats the 2 lists
-print(a1+a2)  #adds the list items like a vector
+# print(l1+l2)  #concats the 2 lists
+# print(a1+a2)  #adds the list items like a vector
 
-m1=np.array([1,2,3])
-m2=np.array([[1],[2]])
-print(m1+m2)            #adds 1 to each item in m1 and again adds 2 so retruns a 2*3 array
-print(np.sqrt(m1))      #square roots each item
-print(np.sin(m1))
-print(np.cos(m1))
-print(np.log10(m1))
+# m1=np.array([1,2,3])
+# m2=np.array([[1],[2]])
+# print(m1+m2)            #adds 1 to each item in m1 and again adds 2 so retruns a 2*3 array
+# print(np.sqrt(m1))      #square roots each item
+# print(np.sin(m1))
+# print(np.cos(m1))
+# print(np.log10(m1))
+
+''' Array Methods'''
+a=np.array([1,2,3])
+a=np.append(a,[7,8,9])
+a=np.insert(a,3,[4,5,6])   #insert into array, position-1, what to insert
+print(a)
+
+b=np.array([[1,2,3],
+            [4,5,6]])
+b=np.delete(a,1)     #deletse element at index 1 which is 2
+b=np.delete(a,4)     #deletes element at index 1 which is 5
+
+c=np.delete(b,0,0)    #deletes entire 0th row, keep last parameter 0 to delete row
+c=np.delete(b,1,0)    #deletes entire 1st row which is 1,2,3
+c=np.delete(b,1,1)    #deletes entire 1st column which is 2 and 5
+
