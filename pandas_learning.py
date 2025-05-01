bfb8c105333d91cd180aebd0925cd8204eecb811
Suprim-Karki <shupreem1911@gmail.com>
2025-05-01 16:42:15 +0545

    Files to work on for learning pandas

---
+++ pandas_learning.py
@@ -0,0 +1,14 @@
+import pandas as pd
+values=[10,20,30]
+s=pd.Series(values)
+print(s)
+
+''' Series: A one dimensional array that can hold any data type: integer, float, string or even Python objects
+            It is often used to track patterns or changes over time, such as daily temperatures, stock prices, etc.
+
+    Dataframe: A dataframe is a 2 dimensional labeled data structure in Pandas, similar to a table in database, an excel spreadsheet or a SQL table.
+               It consists rows and columns. 
+
+
+
+'''
