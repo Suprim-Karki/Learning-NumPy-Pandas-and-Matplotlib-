2a69a11ed304e62c6a53a18c26f318393e568ac4
Suprim-Karki <shupreem1911@gmail.com>
2025-05-03 18:30:17 +0545

    Updating multiple values

--- updating.py
+++ updating.py
@@ -4,5 +4,25 @@ df=pd.read_json("sample_Data.json")
 ''' Using loc to update values'''
 #df.loc[row_index, "column_name"] = new_value
 
-df.loc[0,"Price"]=10.00
+# df.loc[0,"Price"]=10.00
+# print(df)
+
+''' Updating multiple values '''
+df["Price"]=500
 print(df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
