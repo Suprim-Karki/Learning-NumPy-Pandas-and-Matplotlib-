64981e3281565d35c0384f76b63aac64720f4613
Suprim-Karki <shupreem1911@gmail.com>
2025-05-04 22:10:21 +0545

    Inner join

--- merging_and_joining.py
+++ merging_and_joining.py
@@ -2,13 +2,17 @@ import pandas as pd
 
 #consumer data frame
 df_c = pd.DataFrame({
-    "Customer ID":[1,2,3],
-    "Name":["A","B","C","D"]
+    "CustomerID":[1,2,3],
+    "Name":["A","B","C"]
 })
 
 #orders data frame
 df_o = pd.DataFrame({
-    "Customer ID":[1,2,4],
+    "CustomerID":[1,2,4],
     "Order_amount" :[100,200,300]
 })
 
+'''Inner join'''
+# joins only the intersection(same) values
+df_inner=pd.merge(df_c,df_o,on="CustomerID",how="inner")
+print(df_inner)
