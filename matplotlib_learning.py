10d81714ecbf10d1f44bfd42a97a059a76c5abed
Suprim-Karki <shupreem1911@gmail.com>
2025-05-05 21:28:51 +0545

    Title, xlabel and ylabel

--- matplotlib_learning.py
+++ matplotlib_learning.py
@@ -1,7 +1,17 @@
 import matplotlib.pyplot as plt
 
-x=[1,2,3,4]
-y=[10,20,15,30]
+# x=[1,2,3,4]
+# y=[10,20,15,30]
+
+# plt.plot(x,y)
+# plt.show()
+
+'''Title, xlabel, ylabel'''
+x=["Mon","Tue","Wed","Thu","Fri"]
+y=[18,7,10,25,4]
 
 plt.plot(x,y)
+plt.title("Sales of the week")
+plt.xlabel("Days")
+plt.ylabel("Sales")
 plt.show()
