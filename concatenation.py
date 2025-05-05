17c1a1282f9a833b5526a3ea95e523e97d53d2e0
Suprim-Karki <shupreem1911@gmail.com>
2025-05-05 20:57:52 +0545

    Data to concat

---
+++ concatenation.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+'''Concatenation'''
+
+# region-1
+df_region1=pd.DataFrame({
+    "ID":[1,2],
+    "Name":["A","B"]
+})
+
+df_region2=pd.DataFrame({
+    "ID":[3,4],
+    "Name":["C","D"]
+})
