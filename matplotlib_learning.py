146a8bcd166998a1d02f39b7ba2ea7a89806df5f
Suprim-Karki <shupreem1911@gmail.com>
2025-05-08 21:42:26 +0545

    Subplots with width and height syntax example

--- matplotlib_learning.py
+++ matplotlib_learning.py
@@ -105,7 +105,21 @@ Syntax: plt.plot(x,y,color="color_name",linestyle="linestyle,linewidth=value, ma
 # plt.show()
 
 '''Subplot with width and height'''
-Syntax: fig,ax=plt.subplots(nrows,ncols,figsize=(width,height))
+# Syntax: fig,ax=plt.subplots(nrows,ncols,figsize=(width,height))
+
+fig,ax=plt.subplots(1,2,figsize=(10,5))
+
+x=[1,2,3,4]
+y=[10,20,15,25]
+
+ax[0].plot(x,y)
+ax[0].set_title("Line plot")
+
+ax[1].bar(x,y)
+ax[1].set_title("Bar plot")
+plt.tight_layout()
+plt.show()
+
 
 
 
