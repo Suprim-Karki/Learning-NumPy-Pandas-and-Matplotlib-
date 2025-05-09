c96bdfa942cbd8af11ab529c6e7b9357834a0009
Suprim-Karki <shupreem1911@gmail.com>
2025-05-09 21:58:09 +0545

    Q1 and Q2

--- warmup_exercise.py
+++ warmup_exercise.py
@@ -5,3 +5,12 @@ dict1={
     'marks':[90,98,85,70,95,79,82],
     'gender':['Male','Female','Male','Female','Female','Male','Male']
 }
+
+df=pd.DataFrame(dict1)
+print(df)
+
+''' Q1: Display top 3 Rows of dataset'''
+print(df.head(3))
+
+''' Q2: Display last 3 Rows of dataset'''
+print(df.tail(3))
