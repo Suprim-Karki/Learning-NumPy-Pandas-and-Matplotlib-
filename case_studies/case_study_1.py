9169e40c78d949a5a52674d117a49d62913490cd
Suprim-Karki <shupreem1911@gmail.com>
2025-05-13 21:43:16 +0545

    Q2

--- case_studies/case_study_1.py
+++ case_studies/case_study_1.py
@@ -3,4 +3,8 @@ import pandas as pd
 df=pd.read_csv("Ecommerce Purchases")
 
 '''Q1: Check null values in the dataset'''
-print(df.isnull().sum())
+# print(df.isnull().sum())
+
+'''Q2: Highest and lowest purchase price'''
+print(df['Purchase Price'].max())
+print(df['Purchase Price'].min())
