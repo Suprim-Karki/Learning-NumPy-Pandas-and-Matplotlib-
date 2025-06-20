8436c3be5148164b102aaa2c5d6af1a1944f2ba8
Suprim-Karki <shupreem1911@gmail.com>
2025-06-20 21:50:57 +0545

    Q2

--- case_studies/case_study_3.py
+++ case_studies/case_study_3.py
@@ -4,4 +4,8 @@ df = pd.read_csv("adult.csv", encoding='latin1', sep=',', engine='python', error
 
 
 '''Q1: Display the first 10 rows of the dataset.'''
-print(df.head(10))
+# print(df.head(10))
+
+'''Q2: What are the column names and how many are there?'''
+print(df.columns)
+print("Total columns:", len(df.columns))
