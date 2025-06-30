2a7f8809804214cca89f7797467fc3be775a81c0
Suprim-Karki <shupreem1911@gmail.com>
2025-06-30 19:47:14 +0545

    Q2

--- case_studies/case_study_4.py
+++ case_studies/case_study_4.py
@@ -5,3 +5,7 @@ df = pd.read_csv("googleplaystore.csv")
 
 '''Q1: Display the first 10 rows of the dataset.'''
 # print(df.head(10))
+
+'''Q2: What are the column names and how many are there?'''
+print(df.columns)
+print("Total columns:", len(df.columns))
